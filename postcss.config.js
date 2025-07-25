d571d92b5ac772fa659e32d9febdb5547634fce0
Kaigama Barka <solomonbledol@gmail.com>
2025-07-25 15:43:28 +0000

    Fix: Tailwind PostCSS plugin requirement

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {}, // This line is now handled by the new plugin
     autoprefixer: {},
+    // Add the new plugin here:
+    '@tailwindcss/postcss': {}, // This is the new line
   },
 }
